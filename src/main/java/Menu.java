2a8ba59d79457615a25e4cf8f081532e837758fd
RedEyes44 <riccardotonetto06@gmail.com>
2025-03-20 18:25:17 +0100

    Update Menu.java

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,8 +1,5 @@
+package mine;
 
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Menu {
@@ -117,30 +114,5 @@ public class Menu {
 		
 		setSceltaDeposito(scelta);
 	}
-	
-	public static void fun(int x) {
-		x=5;
-	}
-	
-	public static void main(String args[]) throws FileNotFoundException {
-		Scanner input = new Scanner(new File("prova.txt"));
-		File nuovo = new File("nuovo.txt");
-		PrintWriter pw = new PrintWriter(nuovo);
-		int c=0;
-		String ciao = "suca";
-		while(c<10) {
-			pw.println(ciao);
-			
-			c++;
-			
-		}
-		
-		
-		input.close();
-		pw.close();
-	
-	
-	
-	
-	}
+
 }
